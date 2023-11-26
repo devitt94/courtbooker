a3470e5cdd03f4fbaa66d9e291eea4d57157995c
devittr <devittr@tcd.ie>
2023-11-26 22:33:34 +0000

    Add datetime query params

--- src/app/frontend.py
+++ src/app/frontend.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from fastapi import APIRouter, Query, Request
 from fastapi.responses import HTMLResponse
@@ -18,11 +19,18 @@ templates = Jinja2Templates(directory="./templates")
 
 
 @router.get("/courts", response_class=HTMLResponse)
-def get_courts(request: Request, venues: list[str] = Query(None)):
-    if isinstance(venues, str):
-        venues = [venues]
+def get_courts(
+    request: Request,
+    venues: list[str] = Query(None),
+    start_time_gte: datetime = None,
+    start_time_lte: datetime = None,
+):
     logging.warning(f"Venues: {venues}")
-    court_sessions = get_court_sessions(venues=venues)
+    court_sessions = get_court_sessions(
+        venues=venues,
+        start_time_after=start_time_gte,
+        start_time_before=start_time_lte,
+    )
 
     return templates.TemplateResponse(
         "courts.html", {"request": request, "courts": court_sessions}
