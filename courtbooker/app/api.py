02298f69404845b7dd84866f8a2ea0a8cfcc5b40
Rory Devitt <rorytdevitt@gmail.com>
2024-08-07 22:13:54 +0100

    Update frontend to allow filtering for multi courts

--- courtbooker/app/api.py
+++ courtbooker/app/api.py
@@ -17,11 +17,13 @@ def courts(
     venues: list[str] | None = None,
     start_time_after: datetime | None = None,
     start_time_before: datetime | None = None,
+    only_multiple_sessions: bool = False,
 ):
     court_sessions = get_court_sessions(
         venues=venues,
         start_time_after=start_time_after,
         start_time_before=start_time_before,
+        only_multiple_sessions=only_multiple_sessions,
     )
 
     return {
